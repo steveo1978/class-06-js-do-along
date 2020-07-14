57df320e94363ce078d59affe8bcf85ea6efb63b
steveo1978 <steve.r.marical@outlook.com>
2020-07-13 21:22:08 -0700

    fun stuff

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function sayHello(){
+  console.log('Hello world!');  
+}
+
+var sayHelloCookie = function(){
+  console.log('hello cookie!');
+
+function sayHelloUser(name){
+  console.log('hello' +name);
+       }
+}
+
+function addUsername() {
+    var name = prompt('what is your name');
+
+    return document.write('<h3>' + message +'</h3>');
+}
